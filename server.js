4a74d19ed6f711523368511ea2e595c498c5a2fe
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-06-29 13:10:28 -0500

    updated server

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 'use strict';
 
 let fs = require('fs');
+let path = require('path');
 let bodyParser = require('body-parser');
 let config = require('./knexfile.js')['development'];
 let express = require('express');
 let app = express();
 let port = process.env.PORT || 8000;
 let knex = require('knex')(config);
+let morgan = require('morgan');
 let assassins = require('./routes/assassins');
 // let contracts = require('./routes/contracts');
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.disable('x-powered-by');
+app.use(morgan('short'));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
 
 app.use(assassins);
 // app.use(contracts);
