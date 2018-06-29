b1c80ad4e1fbcd2b216fda1df121dc958a95a4ba
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-06-29 13:09:52 -0500

    updated public

---
+++ public/assassinsFetch.js
@@ -0,0 +1,46 @@
+(function () {
+    console.log('What in the hell?');
+    let link = 'http://localhost:8000/';
+    let _rating = document.getElementById('_rating');
+    let _kills = document.getElementById('_kills');
+    let _price = document.getElementById('_price');
+    let _age = document.getElementById('_age');
+    let _weapon = document.getElementById('_weapon');
+    let _contact = document.getElementById('_contact');
+    let _addButton = document.getElementById('_addButton');
+
+
+    document.addEventListener('DOMContentLoaded', function (event) {
+        console.log('What in the hell part 2');
+        fetch(link, {cors: 'no-cors'})
+            .then(response => response.json())
+            .then(data => {
+                console.log('Why you no display my:', data);
+
+                let rating = data.main.rating;
+                let kills = data.main.kills;
+                let price = data.main.price;
+                let age = data.main.age;
+                let weapon = data.main.weapon;
+                let contact = data.main.contact;
+
+                _rating.setAttribute('value', rating);
+                _kills.setAttribute('value', kills);
+                _price.setAttribute('value', price);
+                _age.setAttribute('value', age);
+                _weapon.setAttribute('value', weapon);
+                _contact.setAttribute('value', contact);
+
+                _rating.innerHTML = (`Rating: ${rating}`);
+                _kills.innerHTML = (`Kills: ${kills}`);
+                _price.innerHTML = (`Price: ${price}`);
+                _age.innerHTML = (`Age: ${age}`);
+                _weapon.innerHTML = (`The Weapon: ${weapon}`);
+                _contact.innerHTML = (`Contact: ${contact}`);
+
+            })
+            .catch(error => console.log(error));
+    })
+
+
+})();
