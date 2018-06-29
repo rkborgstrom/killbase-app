38b01a9a38dc6e411e1c6082bec01069d738f258
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-06-29 10:50:16 -0500

    routes updates

--- routes/assassins.js
+++ routes/assassins.js
@@ -1,16 +1,16 @@
-'use strict';
+// 'use strict';
 
-let fs = require('fs');
-let knex = require('../knex');
+// let fs = require('fs');
+// let knex = require('../knex');
 
-let express = require('express');
-const router = express.Router();
-let port = process.env.PORT || 8000;
-let bodyParser = require('body-parser');
+
+let router = require('express').Router();
+// let port = process.env.PORT || 8000;
+// let bodyParser = require('body-parser');
 
 // router.disable('x-powered-by');
-router.use(express.static('public'));
-router.use(bodyParser.json());
+// router.use(express.static('public'));
+// router.use(bodyParser.json());
 
 router.get('/assassins', (req, res, next) => {
 
@@ -24,7 +24,6 @@ router.get('/assassins', (req, res, next) => {
         })
 });
 
-
 router.get('/assassins/:id', (req, res, next) => {
     knex('assassins')
         .where('id', req.params.id)
@@ -33,7 +32,6 @@ router.get('/assassins/:id', (req, res, next) => {
             if (!assassin) {
                 return next();
             }
-
             res.send(assassin);
         })
         .catch((err) => {
@@ -44,7 +42,7 @@ router.get('/assassins/:id', (req, res, next) => {
 router.post('/assassins', (req, res, next) => {
     knex('assassins')
         .insert({
-            full_name: req.body.full_name,
+            Full_name: req.body.Full_name,
             code_names: req.body.code_names,
             weapon: req.body.weapon,
             age: req.body.age,
@@ -53,7 +51,36 @@ router.post('/assassins', (req, res, next) => {
             kills: req.body.kills
         }, '*')
 
-        .then((artists) => {
+        .then((assassins) => {
+            res.send(assassins[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.patch('/assassins/:id', (req, res, next) => {
+    knex('assassins')
+        .where('id', req.params.id)
+        .first()
+        .then((assassin) => {
+            if (!assassin) {
+                return next();
+            }
+
+            return knex('assassins')
+                .update({
+                    Full_name: req.body.Full_name,
+                    code_names: req.body.code_names,
+                    weapon: req.body.weapon,
+                    age: req.body.age,
+                    price: req.body.price,
+                    rating: req.body.rating,
+                    kills: req.body.kills
+                }, '*')
+                .where('id', req.params.id);
+        })
+        .then((assassins) => {
             res.send(assassins[0]);
         })
         .catch((err) => {
---
+++ routes/contracts.js
@@ -0,0 +1,93 @@
+// 'use strict';
+
+// let fs = require('fs');
+// let knex = require('../knex');
+
+
+let router = require('express').Router();
+// let port = process.env.PORT || 8000;
+// let bodyParser = require('body-parser');
+
+// router.disable('x-powered-by');
+// router.use(express.static('public'));
+// router.use(bodyParser.json());
+
+router.get('/contracts', (req, res, next) => {
+
+    knex('contracts')
+        .orderBy('id')
+        .then((contracts) => {
+            res.send(contracts);
+        })
+        .catch((err) => {
+            next(err);
+        })
+});
+
+router.get('/contracts/:id', (req, res, next) => {
+    knex('contracts')
+        .where('id', req.params.id)
+        .first()
+        .then((contracts) => {
+            if (!contracts) {
+                return next();
+            }
+            res.send(contracts);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.post('/contracts', (req, res, next) => {
+    knex('contracts')
+        .insert({
+            client_id: req.body.client_id,
+            budget: req.body.budget,
+            target_id: req.body.target_id,
+            completed: req.body.completed,
+            assassins_id: req.body.assassins_id,
+        }, '*')
+
+        .then((contracts) => {
+            res.send(contracts[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.patch('/contracts/:id', (req, res, next) => {
+    knex('contracts')
+        .where('id', req.params.id)
+        .first()
+        .then((contracts) => {
+            if (!contracts) {
+                return next();
+            }
+
+            return knex('contracts')
+                .update({
+                    client_id: req.body.client_id,
+                    budget: req.body.budget,
+                    target_id: req.body.target_id,
+                    completed: req.body.completed,
+                    assassins_id: req.body.assassins_id,
+                }, '*')
+                .where('id', req.params.id);
+        })
+        .then((contracts) => {
+            res.send(contracts[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+router.use(function (req, res) {
+    console.error('404 status reached.');
+    res.sendStatus(404);
+});
+
+
+module.exports = router;
