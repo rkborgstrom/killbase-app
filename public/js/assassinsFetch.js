9ab3a7702ab2ebeb44fbc1d42aaf1252064c975f
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-07-02 14:22:22 -0500

    updated

--- public/js/assassinsFetch.js
+++ public/js/assassinsFetch.js
@@ -1,7 +1,7 @@
 (function () {
-    console.log('What in the hell?');
     let link = 'http://localhost:8000/assassins';
     let assassinList = document.getElementById('assassins-list');
+    let _fullName = document.getElementById('Full_name');
     let _rating = document.getElementById('_rating');
     let _kills = document.getElementById('_kills');
     let _price = document.getElementById('_price');
@@ -11,74 +11,95 @@
     let _addButton = document.getElementById('_addButton');
 
     document.addEventListener('DOMContentLoaded', function (event) {
-        // console.log('What in the hell part 2');
         fetch(link)
             .then(response => {
                 console.log('Reponse', response);
                 return response.json()
             })
             .then(assassinsData => {
-                // console.log('Why you no display my:', assassinsData);
                 assassinsData.forEach(function (assassinData) {
-                    // Manually create EACH element you need to display assassin data. Append
-                    // each child element to the assassin section. When done, append assassin section
-                    // to the assassinList section.
 
-                    let assassinImg = document.createElement('img');
-                    assassinImg = assassinData.image - container;
-                    assassinSection.appendChild(assassinImg);
+                    let _fullName = assassinData.Full_name;
+                    let _codeName = assassinData.code_names;
+                    let _rating = assassinData.rating;
+                    let _price = assassinData.price;
+                    let _kills = assassinData.kills;
+                    let _age = assassinData.age;
+                    let _weapon = assassinData.weapon;
 
-                    let assassinSection = document.createElement('section');
-                    let assassinName = document.createElement('strong');
-                    assassinName.innerText = assassinData.Full_name;
-                    assassinSection.appendChild(assassinName);
+                    let startingDiv = document.getElementById('assassin-section');
 
-                    let ratingSection = document.createElement('section');
-                    ratingSection = assassinData.rating;
-                    assassinSection.appendChild(ratingSection);
+                    // let allAssassins = document.getElementsByClassName('all-content-assassins');
+                    // startingDiv.appendChild(allAssassins);
 
-                    let killsSection = document.createElement('section');
-                    killsSection = assassinData.kills;
-                    assassinSection.appendChild(killsSection);
+                    let assassinSection = document.createElement('div');
+                    startingDiv.appendChild(assassinSection);
 
-                    let priceSection = document.createElement('section');
-                    priceSection = assassinData.price;
-                    assassinSection.appendChild(priceSection);
+                    let nameDiv = document.createElement('div');
+                    nameDiv.innerText = `Full Name: ${_fullName}`;
+                    startingDiv.appendChild(nameDiv);
 
-                    let ageSection = document.createElement('section');
-                    ageSection = assassinData.age;
-                    assassinSection.appendChild(ageSection);
+                    let row = document.createElement('div');
+                    startingDiv.appendChild(row);
 
-                    let weaponSection = document.createElement('section');
-                    weaponSection = assassinData.weapon;
-                    assassinSection.appendChild(weaponSection);
+                    let codeNameDiv = document.createElement('div');
+                    codeNameDiv.innerHTML = `Code Name: ${_codeName}`;
+                    startingDiv.appendChild(codeNameDiv);
 
-                    let contactSection = document.createElement('section');
-                    contactSection = assassinData.contact;
-                    assassinSection.appendChild(contactSection);
+                    let ratingDiv = document.createElement('div');
+                    ratingDiv.innerText = `Rating: ${_rating}`;
+                    ratingDiv.classList.add("col-6");
+                    ratingDiv.classList.add("col-sm-3");
+                    row.appendChild(ratingDiv);
 
-                    let rating = data.main.rating;
-                    let kills = data.main.kills;
-                    let price = data.main.price;
-                    let age = data.main.age;
-                    let weapon = data.main.weapon;
-                    let contact = data.main.contact;
+                    let priceDiv = document.createElement('div');
+                    priceDiv.innerText = `Price: ${_price}`;
+                    priceDiv.classList.add("col-6");
+                    priceDiv.classList.add("col-sm-3");
+                    row.appendChild(priceDiv);
 
-                    _rating.setAttribute('value', rating);
-                    _kills.setAttribute('value', kills);
-                    _price.setAttribute('value', price);
-                    _age.setAttribute('value', age);
-                    _weapon.setAttribute('value', weapon);
-                    _contact.setAttribute('value', contact);
+                    let killsDiv = document.createElement('div');
+                    killsDiv.innerHTML = `Kills: ${_kills}`;
+                    killsDiv.classList.add("col-6");
+                    killsDiv.classList.add("col-sm-3");
+                    row.appendChild(killsDiv);
 
-                    _rating.innerHTML = (`Rating: ${rating}`);
-                    _kills.innerHTML = (`Kills: ${kills}`);
-                    _price.innerHTML = (`Price: ${price}`);
-                    _age.innerHTML = (`Age: ${age}`);
-                    _weapon.innerHTML = (`The Weapon: ${weapon}`);
-                    _contact.innerHTML = (`Contact: ${contact}`);
+                    let ageDiv = document.createElement('div');
+                    ageDiv.innerText = `Age: ${_age}`;
+                    ageDiv.classList.add("col-6");
+                    ageDiv.classList.add("col-sm-3");
+                    row.appendChild(ageDiv);
 
-                    assassinList.appendChild(assassinSection);
+                    // let weaponDiv = document.createElement('div');
+                    // weaponDiv.innerHTML = `Weapon: ${_weapon}`;
+                    // startingDiv.appendChild(weaponDiv);
+
+                    // let contactSection = document.createElement('section');
+                    // contactSection = assassinData.contact;
+                    // assassinSection.appendChild(contactSection);
+
+                    // let rating = data.main.rating;
+                    // let kills = data.main.kills;
+                    // let price = data.main.price;
+                    // let age = data.main.age;
+                    // let weapon = data.main.weapon;
+                    // let contact = data.main.contact;
+
+                    // _rating.setAttribute('value', rating);
+                    // _kills.setAttribute('value', kills);
+                    // _price.setAttribute('value', price);
+                    // _age.setAttribute('value', age);
+                    // _weapon.setAttribute('value', weapon);
+                    // _contact.setAttribute('value', contact);
+
+                    // _rating.innerHTML = (`Rating: ${rating}`);
+                    // _kills.innerHTML = (`Kills: ${kills}`);
+                    // _price.innerHTML = (`Price: ${price}`);
+                    // _age.innerHTML = (`Age: ${age}`);
+                    // _weapon.innerHTML = (`The Weapon: ${weapon}`);
+                    // _contact.innerHTML = (`Contact: ${contact}`);
+
+                    // assassinList.appendChild(assassinSection);
                 })
 
             })
